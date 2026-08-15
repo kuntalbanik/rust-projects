4bc3b43414ada24617022190fca7fe654da86753
kuntalbanik <deisiduke1986@gmail.com>
2026-08-15 23:50:06 +0530

    Debugging setup

--- practice_code/src/main.rs
+++ practice_code/src/main.rs
@@ -1,7 +1,29 @@
-fn main() {
-  let result = mystery();
+// Recursion function
+// call itself
+fn countdown(seconds: i32) {
+    if seconds == 0 {
+        println!("Blastoff!");
+    } else {
+        println!("{seconds} seconds to blastoff!");
+        countdown(seconds - 1);
+    }
 }
 
-fn mystery() {
-    println!("Hi there");
+fn main() {
+    //   let result = mystery();
+    let number = 10;
+
+    // Match statement with multiple values and condition
+    match number {
+        value if value % 2 == 0 => println!("{value} is even number"),
+        x if x % 2 != 0 => println!("{x} is an odd number"),
+        _ => unreachable!(),
+    }
+
+    /*
+
+
+    */
+    // Call recursion function
+    countdown(number);
 }
